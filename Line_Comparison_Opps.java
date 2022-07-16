add23138f25f24e4fba43a3966175209c30fa13a
vishal5394 <vishaljadyal@gmail.com>
2022-07-16 19:35:03 +0530

    Added: Welcome Line Comparison computation

---
+++ Line_Comparison_Opps.java
@@ -0,0 +1,12 @@
+package com.Bridgelabz.Emp;
+
+public class Line_Comparison_Opps {
+	void Welcome () {
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+public static void main(String[] args) {
+	
+	Line_Comparison_Opps obj = new Line_Comparison_Opps();
+	obj.Welcome();
+}
+}
